d543a892a1f7167f4fd764a18fc2fcac26335e2d
Sam Dods <sam@theappbusiness.com>
2014-03-21 11:46:21 +0000

    Initial Commit

---
+++ MacTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MacTest
+//
+//  Created by Sam Dods on 21/03/2014.
+//  Copyright (c) 2014 Sam Dods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
